7ed7adbac0326e1c1685568f6c433934cc3b3141
YeongHyeonHeo <djdjfug@naver.com>
2025-12-07 03:27:54 +0900

    [Fix] success 페이지 post url 수정

--- app/points/success/page.tsx
+++ app/points/success/page.tsx
@@ -23,7 +23,7 @@ export default function SuccessPage() {
 
     async function approve() {
       try {
-        await axios.post("/api/payments/approve", {
+        await axios.post("/api/v1/payment", {
           paymentKey,
           orderId,
           amount: Number(amount),
