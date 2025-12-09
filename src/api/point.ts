19a03d1a4b00ea37888755ab7e762f24bd31b778
YeongHyeonHeo <djdjfug@naver.com>
2025-12-09 11:43:33 +0900

    [Feat] 포인트 사용 내역 types, api 파일 추가

--- src/api/point.ts
+++ src/api/point.ts
@@ -1,5 +1,6 @@
 import { api } from "./api";
 import { PointResponse } from "../types/point";
+import { PaidOrder } from "../types/point";
 
 /**
  * @file src/api/point.ts
@@ -8,7 +9,14 @@ import { PointResponse } from "../types/point";
  * @description 유저의 포인트와 관련된 API 함수들이 있는 파일입니다.
  */
 
+// 보유 포인트 조회
 export async function fetchMyPoints(): Promise<number> {
   const response = await api.get<{ data: PointResponse }>("/api/points");
   return response.data.data.points; // 숫자만 반환
+}
+
+// 포인트 사용 내역 조회
+export async function fetchPaidOrders(): Promise<PaidOrder[]> {
+  const res = await api.get<{ data: PaidOrder[] }>("/api/orders/paid");
+  return res.data.data;
 }
--- src/types/point.ts
+++ src/types/point.ts
@@ -4,6 +4,18 @@
  * @since 2025-12-03
  * @description 유저의 포인트와 관련된 타입 값을 정의하는 파일입니다.
  */
+
+// 보유 포인트
 export interface PointResponse {
   points: number;
+}
+
+// 포인트 사용 내역
+export interface PaidOrder {
+  orderId: number;
+  userId: number;
+  snackName: string;
+  totalPrice: number;
+  orderStatus: string;
+  orderTime: string; // ISO 날짜 문자열
 }
