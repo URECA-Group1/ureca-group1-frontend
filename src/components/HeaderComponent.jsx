1ab1df1ab374913cd3c682972b0edb4692889d4a
HyeongseoShin <tlsgudtj123@gmail.com>
2025-12-13 13:10:08 +0900

    [Feat] 리프레시 토큰 추가로 인한 프론트 코드 수정 완료 (#57)

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,4 +1,5 @@
 "use client"; // 반드시 필요
+import { api } from "../api/api";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
@@ -8,21 +9,31 @@ export default function HeaderComponent() {
   const router = useRouter();
 
   useEffect(() => {
-    // 쿠키에서 token 가져오기
-    const token = Cookies.get("token");
-    if (token) {
-      localStorage.setItem("token", token);
-      Cookies.remove("token");
+    // 쿠키에서 access 가져오기
+    const access = Cookies.get("access");
+    const refresh = Cookies.get("refresh");
+
+    if (access) {
+      localStorage.setItem("access", access);
+      Cookies.remove("access");
     }
 
     // 토큰 존재 여부로 로그인 상태 결정
-    setIsLogin(!!localStorage.getItem("token"));
+    setIsLogin(!!localStorage.getItem("access"));
   }, []);
 
-  const doLogout = () => {
-    localStorage.removeItem("token");
-    setIsLogin(false); // 상태 업데이트 → UI 자동 갱신
-    router.refresh(); // App Router에서는 새로고침 대신 refresh
+  const doLogout = async () => {
+    try {
+      await api.post("api/members/logout");
+    } catch (error) {
+      console.error("로그아웃 중 서버 에러 발생", error);
+    } finally {
+      localStorage.removeItem("access");
+      Cookies.remove("refresh");
+      setIsLogin(false); // 상태 업데이트 → UI 자동 갱신
+      router.refresh(); // App Router에서는 새로고침 대신 refresh
+    }
+    
   };
 
   return (
